10aae312d50df1a8f69512d2d06824caad8f29b1
Nikita <Nikita@DESKTOP-ERTFKKH>
2022-06-18 22:12:30 +0530

    First commit

---
+++ SpringIOC1/src/main/java/com/cg/ioc/Mobile.java
@@ -0,0 +1,30 @@
+package com.cg.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		/*Airtel a = new Airtel();
+		a.calling();
+		a.data();
+		
+		Jio a1 = new Jio();
+		a1.calling();
+		a1.data();*/
+		
+		@SuppressWarnings("resource")
+		ApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+		/*Sim a=c.getBean(Sim.class);
+		a.calling();
+		a.data();*/
+		
+		Sim j=c.getBean(Sim.class);
+		j.calling();
+		j.data();
+		System.out.println("The program is executed.....!");
+
+	}
+
+}
